977d50884b75b837c9691090c5d269f1b105093c
Bemdell5 <Bemdell5@DESKTOP-R6DP7I7>
2018-07-07 13:23:59 +0300

    Giris session bean eklendi.

---
+++ src/main/java/com/bilisimegitim/rehberweb/session/GirisFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bilisimegitim.rehberweb.session;
+
+import com.bilisimegitim.rehberweb.entity.Giris;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Bemdell5
+ */
+@Stateless
+public class GirisFacade extends AbstractFacade<Giris> {
+
+    @PersistenceContext(unitName = "com.bilisimegitim_RehberWeb_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public GirisFacade() {
+        super(Giris.class);
+    }
+    
+}
